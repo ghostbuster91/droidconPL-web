292064f78300029b7bfd5bc7e309c11e03729dfa
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2016-10-27 15:26:32 +0200

    Assets folder fix

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 
-
 @Component({
   selector: 'my-app',
   template: require('./app.component.pug'),
--- app/footer/footer.component.ts
+++ app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// import facebookImage = require("assets/images/facebook.svg");
+
 @Component({
   selector: 'app-footer',
   template: require('./footer.component.pug'),
@@ -10,37 +12,37 @@ export class FooterComponent {
   socialAssets = {
     "droidconGlobal": {
       "href": "",
-      "imgSrc": require("../../assets/DroidCon_Logo.png"),
+      "imgSrc": require("../../assets/images/DroidCon_Logo.png"),
       "alt": "droidcon global logo"
     },
     "facebook": {
       "href": "https://www.facebook.com/droidconpl",
-      "imgSrc": require("../../assets/facebook.svg"),
+      "imgSrc": require("../../assets/images/facebook.svg"),
       "alt": "facebook logo"
     },
     "twitter": {
       "href": "https://twitter.com/droidconkr",
-      "imgSrc": require("../../assets/twitter.svg"),
+      "imgSrc": require("../../assets/images/twitter.svg"),
       "alt": "twitter logo"
     },
     "github": {
       "href": "https://github.com/droidconpl",
-      "imgSrc": require("../../assets/github.svg"),
+      "imgSrc": require("../../assets/images/github.svg"),
       "alt": "github logo"
     },
     "googlePlus": {
       "href": "https://plus.google.com/u/3/+droidconKrak%C3%B3wPL",
-      "imgSrc": require("../../assets/google+.svg"),
+      "imgSrc": require("../../assets/images/google+.svg"),
       "alt": "Google Plus logo"
     },
     "youTube": {
       "href": "https://www.youtube.com/watch?v=Dg-dbrP9PaM&list=PLvpKlOXx1MBMfL4llJOi0VX6LcQVP0Su0",
-      "imgSrc": require("../../assets/youtube.svg"),
+      "imgSrc": require("../../assets/images/youtube.svg"),
       "alt": "YouTube logo"
     }
   };
-
   constructor() {
-
+    // console.log(this.imgSrc);
   }
+
 }
