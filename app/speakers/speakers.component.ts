d153a0d4afde7179e9f2952eae077d1e964d4e0f
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2016-11-03 13:30:23 +0100

    Add Speaker modal

--- app/speakers/speakers.component.ts
+++ app/speakers/speakers.component.ts
@@ -8,7 +8,16 @@ import { SpeakersService } from './speakers.service';
 })
 
 export class SpeakersComponent {
+  imgSrc: string = '../../assets/photos/speakers/';
   speakers: string[] = [];
+  modal: string[] = [];
+
+  websiteIcon: string = require('../../assets/images/website.svg');
+  facebookIcon: string = require('../../assets/images/facebook.svg');
+  twitterIcon: string = require('../../assets/images/twitter.svg');
+  githubIcon: string = require('../../assets/images/github.svg');
+  linkedInIcon: string = require('../../assets/images/linkedin.svg');
+  googlePlusIcon: string = require('../../assets/images/google+.svg');
 
   constructor(speakersService : SpeakersService) {
     this.speakers = speakersService.getSpeakers();
@@ -17,4 +26,9 @@ export class SpeakersComponent {
   speakerImg(img: string) {
     return require('../../assets/photos/speakers/' + img);
   }
+
+  setSpeaker(speaker: any) {
+    this.modal = speaker;
+  }
+
 }
