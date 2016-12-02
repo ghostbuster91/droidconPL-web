e1c81d801e6c1f21b4fa42fc7fcbe8d07132230a
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2016-12-02 23:16:05 +0100

    Separate sponsors

--- app/sponsors/sponsors.component.ts
+++ app/sponsors/sponsors.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SponsorsService } from './sponsors.service';
 
+var _ = require('lodash');
+
 @Component({
   selector: 'sponsors',
   template: require('./sponsors.component.pug'),
@@ -8,10 +10,14 @@ import { SponsorsService } from './sponsors.service';
 })
 
 export class SponsorsComponent {
-  sponsors: string[] = [];
+  bronzeSponsors: string[] = [];
+  travelPartners: string[] = [];
+  productPartners: string[] = [];
 
   constructor(sponsorsService : SponsorsService) {
-    this.sponsors = sponsorsService.getSponsors();
+    this.bronzeSponsors = sponsorsService.getByCategory('Bronze Sponsor');
+    this.productPartners = sponsorsService.getByCategory('Product');
+    this.travelPartners = sponsorsService.getByCategory('Travel');
   }
 
   sponsorLogo(logo: string) {
--- app/sponsors/sponsors.service.ts
+++ app/sponsors/sponsors.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 
+var _ = require('lodash');
+
 @Injectable()
 
 export class SponsorsService{
 
   getSponsors() : string[]{
     return require('../../model/sponsors.json');
+  }
+
+  getByCategory(cat: string) : string[] {
+    let sponsorCategory: string [] = [];
+    sponsorCategory = _.filter(require('../../model/sponsors.json'), ['sponsorPackage', cat]);
+    return sponsorCategory;
   }
 
 }
